26b3905cbc57d4b2105e21e3eeaa47a302da5588
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-03-18 22:57:40 -0500

    api/plaid.js is now able to acecs client on plaid

--- pages/api/plaid.js
+++ pages/api/plaid.js
@@ -1,14 +1,16 @@
 import nextConnect from 'next-connect';
 
-// import { Client, environments } from 'plaid';
+import { Client, environments } from 'plaid';
 
-// const { PLAID_CLIENT_ID, PLAID_SECRET } = process.env;
+const { PLAID_CLIENT_ID, PLAID_SECRET } = process.env;
 
-// const client = new Client({
-// 	clientID: PLAID_CLIENT_ID,
-// 	secret: PLAID_SECRET,
-// 	env: environments.sandbox,
-// });
+const client = new Client({
+	clientID: PLAID_CLIENT_ID,
+	secret: PLAID_SECRET,
+	env: environments.sandbox,
+});
+
+console.log(client);
 
 const handler = nextConnect();
 
