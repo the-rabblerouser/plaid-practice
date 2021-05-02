8af73c91b2890466c2f6c09744c382639fa4a530
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-05-02 01:04:48 -0500

    added axios

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,31 @@
 import Head from 'next/head';
+import { useState, useEffect } from 'react';
+
+import axios from 'axios';
+
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+	const [token, setToken] = useState(null);
+
+	useEffect(() => {
+		async () => {
+			const res = await axios.post('localhost:3000/api/create_link_token');
+			const data = res.data.link_token;
+			setToken(data);
+		};
+	}, []);
+
+	console.log(token);
+
 	return (
 		<div className={styles.container}>
-			<Head>
-				<title>plaid-practice</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
-			<div>Hello World</div>
+			<>
+				<div className="App">
+					//Passes down created Link token to Link component
+					{/* <Link token={token} /> */}
+				</div>
+			</>
 		</div>
 	);
 }
